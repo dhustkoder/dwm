78ad813810b2c6e918051cf84c8ddded9a071b64
Rafael Moura (dhustkoder) <rafaelmoura.dev@gmail.com>
2022-04-26 15:43:42 -0300

    added dwmshot shortcut
    Signed-off-by: Rafael Moura (dhustkoder) <rafaelmoura.dev@gmail.com>

--- dhust-nx-config.h
+++ dhust-nx-config.h
@@ -63,11 +63,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray4, "-sb", col_dark_green, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *dwmshot[]  = { "dwmshot", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Print,  spawn,          {.v = dwmshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
